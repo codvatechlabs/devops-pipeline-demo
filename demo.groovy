e5f41adb9b34466b51fcbe7f7df8220a23700069
codvatechlabs <78929919+codvatechlabs@users.noreply.github.com>
2022-02-16 20:28:23 +0530

    Create demo.groovy

---
+++ demo.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Version_Check') {
+            steps {
+                echo 'Version Check '
+                sh 'aws s3 ls'
+                sh 'terraform -version'
+            }
+        }
+        stage('Terraform_Create') {
+            steps {
+                echo 'Build Infra using terraform'
+                checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/codvatechlabs/tf-jenkins.git']]]
+                sh 'ls'
+                sh 'pwd'
+                sh 'terraform init'
+                sh 'terraform plan'
+                sh 'terraform apply -auto-approve'
+                
+            }
+        }
+    }
+}
